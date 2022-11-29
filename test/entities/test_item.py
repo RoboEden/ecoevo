4bd78dcd67a6b0bdba768cea19c1430a42d0c869
arbingchen <arbingchen@chaocanshu.ai>
2022-11-29 19:01:59 +0800

    Fix item test

--- test/entities/test_item.py
+++ test/entities/test_item.py
@@ -1,4 +1,4 @@
-from ecoevo.entities.items import ItemUtils
+from ecoevo.entities import items
 import argparse
 
 
@@ -11,6 +11,8 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    args = parse_args()    
-    item_attrs = ItemUtils.read_csv(args.csv_path, sep=",")
-    print(item_attrs)
+    args = parse_args()
+    gold = items.Gold(reserve=10)
+    print(gold)
+
+    
