c8f9c78f07ac0e52a7e00dafe3ee8b042a360a10
weihanshen <weihanshen@chaocanshu.ai>
2022-12-05 06:33:46 +0000

    Add valid_action method to env

--- test/env_test.py
+++ test/env_test.py
@@ -1,5 +1,11 @@
 from ecoevo import EcoEvo
+from rich import print
 
 env = EcoEvo()
 obs, infos = env.reset()
+print(env.num_player)
+action = (('move', 'up'), ('sand', -5), ('gold', 10))
+actions = [action] * env.num_player
+obs, reward, infos, done = env.step(actions)
+print(infos)
 print(obs)
