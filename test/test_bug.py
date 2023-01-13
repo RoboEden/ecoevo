509a65b46fea4710695790cf97e0a1490301fcb1
jingxuanliu <jingxuanliu@chaocanshu.ai>
2023-01-13 16:37:57 +0800

    ADD test

--- test/test_bug.py
+++ test/test_bug.py
@@ -1,20 +1,23 @@
-from helper import Action, Helper, Item
+from helper import Action, Helper, Item, Move
 
 
-class TestBug:
+def test_38():
+    h = Helper().init_points(((0, 0), 0), ).reset().step(
+        (0, ((Action.move, Move.down), None, None)), ).assert_pos_player(((0, 0), 0), ).assert_no_error_log()
 
-    def test_54(self):
-        # trader 和 env 关于action的验证不一致，导致交易和step里面的action_valid不一致
-        h = Helper().init_points(
-            ((8, 8), 0),
-            ((8, 9), 1),
-        ).reset().set_bag(
-            (0, Item.gold, 10),
-            (1, Item.sand, 10),
-        ).step(
-            (0, ((Action.idle, None), (Item.gold, -10), (Item.sand, 10))),
-            (1, ((Action.collect, None), (Item.sand, -10), (Item.gold, 10))),
-        ).assert_bag(
-            (0, Item.sand, 10),
-            (1, Item.gold, 10),
-        )
+
+def test_54():
+    # trader 和 env 关于action的验证不一致，导致交易和step里面的action_valid不一致
+    h = Helper().init_points(
+        ((8, 8), 0),
+        ((8, 9), 1),
+    ).reset().set_bag(
+        (0, Item.gold, 10),
+        (1, Item.sand, 10),
+    ).step(
+        (0, ((Action.idle, None), (Item.gold, -10), (Item.sand, 10))),
+        (1, ((Action.collect, None), (Item.sand, -10), (Item.gold, 10))),
+    ).assert_bag(
+        (0, Item.sand, 10),
+        (1, Item.gold, 10),
+    )
--- test/test_env.py
+++ test/test_env.py
@@ -1,31 +1,48 @@
-from helper import ALL_ITEMS, Action, Helper, Item
+from helper import ALL_ITEMS, Action, Helper, Item, Move
 
 from ecoevo import EcoEvo
 
 
-class TestDemo:
+def test_collect_success():
+    h = Helper().init_points(((8, 8), 0), ).init_tiles(((8, 8), Item.gold, ALL_ITEMS.gold.harvest_num + 1), ).reset()
 
-    def test_collect(self):
-        h = Helper().init_points(((8, 8), 0), ).init_tiles(
-            ((8, 8), Item.gold, ALL_ITEMS.gold.consume_num + 1), ).reset().assert_tiles(
-                ((8, 8), Item.gold, ALL_ITEMS.gold.consume_num + 1), ).step(
-                    (0, ((Action.collect, None), None, None)), ).assert_tiles(((8, 8), Item.gold, 1), )
+    for _ in range(h.env.players[0].ability[Item.gold]):
+        h.assert_tiles(((8, 8), Item.gold, ALL_ITEMS.gold.harvest_num + 1), ).step(
+            (0, ((Action.collect, None), None, None)), )
 
-    def test_trade(self):
-        h = Helper().init_points(
-            ((8, 8), 0),
-            ((8, 9), 1),
-        ).reset().set_bag(
-            (0, Item.gold, 5),
-            (1, Item.sand, 10),
-        ).step(
-            (0, ((Action.idle, None), (Item.gold, -5), (Item.sand, 10))),
-            (1, ((Action.idle, None), (Item.sand, -10), (Item.gold, 5))),
-        ).assert_bag(
-            (0, Item.sand, 10),
-            (1, Item.gold, 5),
-        )
-        transaction_graph = h.info['transaction_graph']
-        assert transaction_graph[(0, 1)] == (Item.gold, 5)
-        assert transaction_graph[(1, 0)] == (Item.sand, 10)
-        assert len(transaction_graph) == 2
+    h.assert_tiles(((8, 8), Item.gold, 1), ).assert_no_error_log()
+    assert h.env.players[0].collect_remain is None
+
+
+def test_move_conflict():
+    from ecoevo.config import MapConfig
+    h = Helper().init_points(
+        ((16, 13), 0),
+        ((16, 15), 1),
+    ).reset().step(
+        (0, ((Action.move, Move.up), None, None)),
+        (1, ((Action.move, Move.down), None, None)),
+    ).assert_no_error_log()
+    assert h.env.gettile((16, 14)).player.id in [0, 1]
+    assert h.env.players[0].pos in [(16, 13), (16, 14)]
+    assert h.env.players[1].pos in [(16, 14), (16, 15)]
+
+
+def test_trade():
+    h = Helper().init_points(
+        ((8, 8), 0),
+        ((8, 9), 1),
+    ).reset().set_bag(
+        (0, Item.gold, 5),
+        (1, Item.sand, 10),
+    ).step(
+        (0, ((Action.idle, None), (Item.gold, -5), (Item.sand, 10))),
+        (1, ((Action.idle, None), (Item.sand, -10), (Item.gold, 5))),
+    ).assert_bag(
+        (0, Item.sand, 10),
+        (1, Item.gold, 5),
+    )
+    transaction_graph = h.info['transaction_graph']
+    assert transaction_graph[(0, 1)] == (Item.gold, 5)
+    assert transaction_graph[(1, 0)] == (Item.sand, 10)
+    assert len(transaction_graph) == 2
